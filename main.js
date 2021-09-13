984df3c76a62c2c301b1192b9101f9b038c5caa1
E - Coders (Aasheesh Agarwal) <83082760+e-coders@users.noreply.github.com>
2021-09-13 06:31:24 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 function understand(value){
-  console.log(value)
-  document.getElementById("checkbox").disabled = "false";
+  var toggle = document.querySelector("#checkbox").disabled;
+  if(toggle === "false"){
+    console.log("Clicked");
+    document.querySelector("#checkbox").disabled = true;
+   else{
+     console.log("Clicked");
+     document.querySelector("#checkbox").disabled = false;
+  document.getElementById("checkbox").disabled = false;
   console.log("Clicked");
 }
 function next_page(){
