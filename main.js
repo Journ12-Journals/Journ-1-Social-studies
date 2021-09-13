139a125752130b8ba162f337766a06db46e2ab0a
Anantjit <anantjitchander@gmail.com>
2021-09-13 20:22:38 +0530

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-function understand(value){
-  console.log(value)
-  document.getElementById("checkbox").disabled = "false";
-  console.log("Clicked");
-}
-function next_page(){
-window.location = "article.html";
-}
