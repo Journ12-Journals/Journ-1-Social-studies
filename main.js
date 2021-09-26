ffe58e0bcf09b3d30c6961f8181f9913db42ba0d
Aarush Goradia <80135238+Super45coder@users.noreply.github.com>
2021-09-26 12:32:45 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,16 +1,8 @@
 
-function understand(value){
-  if(document.getElementById("checkbox").disabled === false){
-        document.getElementById("checkbox").disabled = "true";
-  }
-  if(document.getElementById("checkbox").disabled === true){
-    document.getElementById("checkbox").disabled = "false";
-  }
-
 function understand(){
-  var able = document.getElementById("checkbox").disabled;
-  console.log(able)
-}
+  if (document.getElementById("checkbox").checked == "true"){
+    document.getElementById("understand").disabled = "false";
+  }
 function next_page(){
 window.location = "article.html";
 }
